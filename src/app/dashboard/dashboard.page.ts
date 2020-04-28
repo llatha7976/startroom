12fe54cda3fb53ac3fdf468fe78e4a8e3c3c1381
llatha7976 <kavyasoun96@gmail.com>
2020-04-28 16:25:46 +0530

    commit

---
+++ src/app/dashboard/dashboard.page.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.page.html',
+  styleUrls: ['./dashboard.page.scss'],
+})
+export class DashboardPage implements OnInit {
+
+  constructor(private route: Router) { }
+
+  ngOnInit() {
+  }
+
+  goLogin(){
+    this.route.navigate(['login']);
+  }
+
+  goInitiative(){
+    this.route.navigate(['initiatives']);
+  }
+}
